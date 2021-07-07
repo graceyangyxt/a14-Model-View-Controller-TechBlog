d027fbd8490d6a2cf0d30264ee57d97508b754db
Grace Yang <graceyxt.artdesign@gmail.com>
2021-07-07 00:41:02 -0400

    set up db fro heroku

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-        host:'localhost',
+        host:process.env.JAWSDB_URL ?? 'localhost',
         dialect: 'mysql',
         port:3306,
     }
