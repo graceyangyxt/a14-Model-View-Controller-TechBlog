1b2850585fd592834851d1d730c2a27c3e8ff9bd
Grace Yang <graceyxt.artdesign@gmail.com>
2021-07-07 00:52:09 -0400

    fix DB_HOST

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-        host:process.env.JAWSDB_URL ?? 'localhost',
+        host:process.env.DB_HOST ?? 'localhost',
         dialect: 'mysql',
         port:3306,
     }
